15a2e8104449886a9ece1b36bd3f94a2fc08f7c2
fox766 <1320559652@qq.com>
2025-08-08 23:24:52 +0800

    writing chaincode for order

--- blockchain/chaincode-go/chaincode/module.go
+++ blockchain/chaincode-go/chaincode/module.go
@@ -1,7 +1,35 @@
 package chaincode
 
+import (
+	"time"
+)
+
 type User struct {
-	UserID       string   `json:"userID"`
-	UserName     string   `json:"username"`
-	Balance      float64  `json:"balance"`
+	UserID       string          `json:"userid"`
+	UserName     string          `json:"username"`
+	UserRole     string          `json:"userrole"`
+	Available    float64         `json:"available`
+	Balance      float64         `json:"balance"`
+	//DeviceList   []*EnergyDevice `json:"devicelist"`
+}
+
+type EnergyStatus struct {
+	EnergyPrice  float64 `json:"energyprice"` 
+	Fee          float64 `json:"fee"` 
+}
+
+type EnergyDevice struct {
+	OwnerName       string  `json:"onwer`
+	OwnerID         string  `json:"ownerid"`
+	DeeviceName     string  `json:"devicename"`
+}
+
+type Order struct {
+	ID             string    `json:"id"`
+	PartyA         string    `json:"partyA"`    // seller
+	PartyB         string    `json:"partyB"`    // buyer
+ 	Amount         float64   `json:"amount"`
+	Price          float64   `json:"price"`
+	Status         string    `json:"status"` // CREATED/MATCHED/COMPLETED
+	CreatedAt      time.time `json:"createdAt"`
 }
--- blockchain/chaincode-go/chaincode/smartcontract.go
+++ blockchain/chaincode-go/chaincode/smartcontract.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	//"strconv"
-	//"time"
+	"time"
 
 	"github.com/hyperledger/fabric-contract-api-go/v2/contractapi"
 )
@@ -75,13 +75,42 @@ func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, i
 
 // ========== 核心功能 ==========
 
+// Init the energy price
+func (s *SmartContract) Init(ctx contractapi.TransactionContextInterface) error{
+	status := EnergyStatus{
+		EnergyPrice:  1.0,
+		Fee:          0.01,                 
+	}
 
+	statusJSON, err := json.Marshal(params)
+	if err != nil {
+		return fmt.Errorf("failed to marshal: %v", err)
+	}
 
-//注册用户
-func (s *SmartContract) RegisterUser(ctx contractapi.TransactionContextInterface, userid string, username string, balance float64) error {
+	if err := ctx.GetStub().PutState("ENERGY_STATUS", paramsJSON); err != nil {
+		return fmt.Errorf("failed to save to the world state %v", err)
+	}
+
+	return nil
+}
+
+// update the energy status
+func (s *SmartContract) UpdateEnergyStatus(ctx contractapi.TransactionContextInterface, newstatus *EnergyStatus) error{
+	newStatusJSON, err := json.Marshal(newStatus)
+	if err != nil {
+		return err
+	}
+	return ctx.GetStub().PutState("ENERGY_STATUS", assetJSON)
+}
+
+// register user
+func (s *SmartContract) RegisterUser(ctx contractapi.TransactionContextInterface, userid, username, userrole string, 
+	available, float64, balance float64) error {
 	user := User{
 		UserID:       userid,
 		UserName:     username,
+		UserRole:     userrole,
+		Available:    available,
 		Balance:      balance,	
 	}
 
@@ -94,4 +123,99 @@ func (s *SmartContract) RegisterUser(ctx contractapi.TransactionContextInterface
 		return err
 	}
 	return nil
+}
+
+func (s *SmartContract) RegisterDevice(ctx contractapi.TransactionContextInterface) error {
+
+}
+
+
+func (s *SmartContract) TransferDevice(ctx contractapi.TransactionContextInterface, devicename, newowner string) error {
+
+}
+
+func (s *SmartContract) CreateOrder(ctx contractapi.TransactionContextInterface, id, partyA, partyB, status string, 
+	createdtime time.time, amout, price float64) error {
+	
+	partyB = ""
+	status = "CREATED"
+	order = Order {
+		ID: id,
+		PartyA: partyA,
+		partyB: partyB,
+		Amount: amount,
+		Price: price,
+		Status: status,
+		CreatedAt: createdtime,
+	}
+
+	orderJSON, err := json.Marshal(order)
+	if err != nil {
+		return fmt.Errorf("failed to marshal: %v", err)
+	}
+
+	if err := ctx.GetStub().PutState(id, paramsJSON); err != nil {
+		return fmt.Errorf("failed to save to the world state %v", err)
+	}
+
+	return nil
+}
+
+func (s *SmartContract) GetOrder(ctx contractapi.TransactionContextInterface, id string) (Order, error) {
+	
+	exists, err := s.ItemExists(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		return nil, fmt.Errorf("the order %s does not exist", id)
+	}
+	
+	orderJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	var order Order
+	err = json.Unmarshal(orderJSON, &order)
+	if err != nil {
+		return nil, err
+	}
+
+	return &order, nil
+}
+
+// change the status of order to MATCHED
+func (s *SmartContract) MatchOrder(ctx contractapi.TransactionContextInterface, id, partyB string) error{
+	order, err := s.GetOrder(ctx, id)
+	if err != nil {
+		return fmt.Errorf("get order %s failed", id)
+	}
+	order.PartyB = partyB
+	if order.status != "CREATED" {
+		return fmt.Errorf("Match order failed: %v", err)
+	} 
+	order.status = "MATCHED"
+	orderJSON, err := json.Marshal(order)
+	if err != nil {
+		return fmt.Errorf("failed to marshal: %v", err)
+	}
+
+	if err := ctx.GetStub().PutState(id, paramsJSON); err != nil {
+		return fmt.Errorf("failed to save to the world state %v", err)
+	}
+
+	return nil
+}
+
+// settle the order
+func (s *SmartContract) SettleOrder (ctx contractapi.TransactionContextInterface, )
+
+// used to judge if the order, user, energy device exists or not
+func (s *SmartContract) ItemExists(ctx contractapi.TransactionContextInterface, item string) (bool, error){
+	itemJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+	return itemJSON != nil, nil
 }
